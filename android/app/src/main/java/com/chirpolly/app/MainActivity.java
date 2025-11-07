5410cfeeaf98668af4b6bb46c8a4098f0c9146c8
sunilkk06 <sunil_krishna5@hotmail.com>
2025-11-07 16:55:10 +0530

    chore: Final cleanup and package lock update

---
+++ android/app/src/main/java/com/chirpolly/app/MainActivity.java
@@ -0,0 +1,5 @@
+package com.chirpolly.app;
+
+import com.getcapacitor.BridgeActivity;
+
+public class MainActivity extends BridgeActivity {}
---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'com.chirpolly.app',
+  appName: 'ChirPolly',
+  webDir: 'dist'
+};
+
+export default config;
